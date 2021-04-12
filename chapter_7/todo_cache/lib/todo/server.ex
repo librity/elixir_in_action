afcee2ea2741761c44409806d0aba5c4a1302916
Luis Geniole <luis.geniole@gmail.com>
2021-04-11 21:59:50 -0300

    Create todo_cache project.

---
+++ chapter_7/todo_cache/lib/todo/server.ex
@@ -0,0 +1,30 @@
+defmodule Todo.Server do
+  use GenServer
+
+  alias Todo.List
+
+  @impl GenServer
+  def init(_), do: {:ok, List.new()}
+
+  @impl GenServer
+  def handle_call({:all}, _caller, todo), do: {:reply, List.all(todo), todo}
+  def handle_call({:by_date, date}, _caller, todo), do: {:reply, List.by_date(todo, date), todo}
+
+  def handle_call({:by_title, title}, _caller, todo),
+    do: {:reply, List.by_title(todo, title), todo}
+
+  def handle_call({:by_id, id}, _caller, todo), do: {:reply, List.by_id(todo, id), todo}
+  def handle_call(_bad_request, _caller, todo), do: {:reply, :bad_request, todo}
+
+  @impl GenServer
+  def handle_cast({:add_entry, entry}, todo), do: {:noreply, List.add_entry(todo, entry)}
+  def handle_cast({:update_entry, entry}, todo), do: {:noreply, List.update_entry(todo, entry)}
+
+  def handle_cast({:update_entry, entry_id, updater_fun}, todo),
+    do: {:noreply, List.update_entry(todo, entry_id, updater_fun)}
+
+  def handle_cast({:delete_entry, entry_id}, todo),
+    do: {:noreply, List.delete_entry(todo, entry_id)}
+
+  def handle_cast(_bad_request, todo), do: {:noreply, todo}
+end
---
+++ chapter_7/todo_cache/lib/todo/server/client.ex
@@ -0,0 +1,17 @@
+defmodule Todo.Server.Client do
+  def start, do: GenServer.start(Todo.Server, nil)
+
+  def all(pid), do: GenServer.call(pid, {:all})
+  def by_date(pid, date), do: GenServer.call(pid, {:by_date, date})
+  def entries(pid, date), do: by_date(pid, date)
+  def by_title(pid, title), do: GenServer.call(pid, {:by_title, title})
+  def by_id(pid, id), do: GenServer.call(pid, {:by_id, id})
+
+  def add_entry(pid, entry), do: GenServer.cast(pid, {:add_entry, entry})
+  def update_entry(pid, entry), do: GenServer.cast(pid, {:update_entry, entry})
+
+  def update_entry(pid, entry_id, updater_fun),
+    do: GenServer.cast(pid, {:update_entry, entry_id, updater_fun})
+
+  def delete_entry(pid, entry_id), do: GenServer.cast(pid, {:delete_entry, entry_id})
+end
