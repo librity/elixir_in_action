293ab9631338b9e09627e90c22dce05c28a5e9ed
Luis Geniole <luis.geniole@gmail.com>
2021-04-11 23:17:31 -0300

    Add load test.

---
+++ chapter_7/todo_cache/lib/load_test.ex
@@ -0,0 +1,40 @@
+# Very quick, inconclusive load test
+#
+# Start from command line with:
+#   elixir --erl "+P 2000000" -S mix run -e LoadTest.run
+#
+# Note: the +P 2000000 sets maximum number of processes to 2 millions
+defmodule LoadTest do
+  @total_processes 1_000_000
+  @interval_size 100_000
+
+  def run do
+    {:ok, cache} = Todo.Cache.Client.start()
+
+    interval_count = round(@total_processes / @interval_size)
+    Enum.each(0..(interval_count - 1), &run_interval(cache, make_interval(&1)))
+  end
+
+  defp make_interval(n) do
+    start = n * @interval_size
+    start..(start + @interval_size - 1)
+  end
+
+  defp run_interval(cache, interval) do
+    {time, _} =
+      :timer.tc(fn ->
+        interval
+        |> Enum.each(&Todo.Cache.Client.server_process(cache, "cache_#{&1}"))
+      end)
+
+    IO.puts("#{inspect(interval)}: average put #{time / @interval_size} μs")
+
+    {time, _} =
+      :timer.tc(fn ->
+        interval
+        |> Enum.each(&Todo.Cache.Client.server_process(cache, "cache_#{&1}"))
+      end)
+
+    IO.puts("#{inspect(interval)}: average get #{time / @interval_size} μs\n")
+  end
+end
